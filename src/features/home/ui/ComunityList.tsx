9344a3690b46220c1a99177b3763f7fe61493341
G-jjunny <rudwns9551@naver.com>
2025-12-29 15:51:05 +0900

    feat: 커뮤니티 카드 및 리스트 생성

--- src/features/home/ui/ComunityList.tsx
+++ src/features/home/ui/ComunityList.tsx
@@ -1,5 +1,13 @@
 const ComunityList = () => {
-  return <div>ComunityList</div>;
+  return (
+    <div className=" flex">
+      <div className="">
+        {/* 게시글 타이틀 및 카테고리 */}
+        <p>[당직]</p>
+      </div>
+      <div className="">{/* 게시글 날짜 */}</div>
+    </div>
+  );
 };
 
 export default ComunityList;
--- src/widgets/home/ui/CommunityCard.tsx
+++ src/widgets/home/ui/CommunityCard.tsx
@@ -1,7 +1,10 @@
 import { SquarePlus } from 'lucide-react';
+import { Link } from 'react-router';
 
+import ComunityList from '@/features/home/ui/ComunityList';
 import { Button } from '@/shared/components/ui/button';
-import { Card, CardHeader, CardTitle } from '@/shared/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/shared/components/ui/card';
+import { ROUTES } from '@/shared/constants/routes';
 
 const CommunityCard = () => {
   return (
@@ -9,11 +12,19 @@ const CommunityCard = () => {
       <CardHeader>
         <CardTitle>
           커뮤니티
-          <Button variant="ghost" size="icon">
-            <SquarePlus />
-          </Button>
+          <Link to={ROUTES.COMMUNITY}>
+            <Button variant="ghost" size="icon">
+              <SquarePlus />
+            </Button>
+          </Link>
         </CardTitle>
       </CardHeader>
+      <CardContent>
+        <ComunityList />
+        <ComunityList />
+        <ComunityList />
+        <ComunityList />
+      </CardContent>
     </Card>
   );
 };
